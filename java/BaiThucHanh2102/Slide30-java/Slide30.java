b27ec702fc747a14fa17879f8305b9614eec4635
duynguyen003 <101272919+duynguyen003@users.noreply.github.com>
2023-02-27 19:17:08 +0700

    Add files via upload

---
+++ java/BaiThucHanh2102/Slide30-java/Slide30.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Slide30 {
+    public static void main(String[] args) {
+        int n, soDu, tong = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so nguyen bat ky: ");
+        n = scanner.nextInt();
+        while ( n > 0 ) {
+            soDu = n%10;
+            n = n /10;
+            tong = tong + soDu;
+        }
+        System.out.println("Tong cac chu so =" + tong);
+    }
+}
